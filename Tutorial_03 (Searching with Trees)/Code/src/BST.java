ff320046088a83cf537a961d882b0103ccf1e2be
Nazhim <nazhimkalam@gmail.com>
2021-02-10 14:30:44 +0530

    commit

--- Tutorial_03 (Searching with Trees)/Code/src/BST.java
+++ Tutorial_03 (Searching with Trees)/Code/src/BST.java
@@ -16,12 +16,16 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
     }
 
-    // is the symbol table empty?
+    public BST() {
+        // Initializes an empty symbol table.
+    }
+
+    // Returns true if this symbol table is empty.
     public boolean isEmpty() {
         return size() == 0;
     }
 
-    // return number of key-value pairs in BST
+    // Returns the number of key-value pairs in this symbol table.
     public int size() {
         return size(root);
     }
@@ -32,16 +36,16 @@ public class BST<Key extends Comparable<Key>, Value> {
         else return x.N;
     }
 
-    /***********************************************************************
-     *  Search BST for given key, and return associated value if found,
-     *  return null if not found
-     ***********************************************************************/
-    // does there exist a key-value pair with given key?
+
+    //  Does this symbol table contain the given key?
     public boolean contains(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
         return get(key) != null;
     }
 
-    // return value associated with the given key, or null if no such key exists
+    // Returns the value associated with the given key.
     public Value get(Key key) {
         return get(root, key);
     }
@@ -54,72 +58,98 @@ public class BST<Key extends Comparable<Key>, Value> {
         else return x.val;
     }
 
-    /***********************************************************************
-     *  Insert key-value pair into BST
-     *  If key already exists, update with new value
-     ***********************************************************************/
+    // Inserts the specified key-value pair into the symbol table, overwriting the old
+    // value with the new value if the symbol table already contains the specified key.
+    // Deletes the specified key (and its associated value) from this symbol table
+    // if the specified value is {@code null}
     public void put(Key key, Value val) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls put() with a null key");
+        }
         if (val == null) {
             delete(key);
             return;
         }
         root = put(root, key, val);
-        // assert check();
     }
 
     private Node put(Node x, Key key, Value val) {
-        if (x == null) return new Node(key, val, 1);
+        if (x == null) {
+            return new Node(key, val, 1);
+        }
         int cmp = key.compareTo(x.key);
-        if (cmp < 0) x.left = put(x.left, key, val);
-        else if (cmp > 0) x.right = put(x.right, key, val);
-        else x.val = val;
+        if (cmp < 0) {
+            x.left = put(x.left, key, val);
+        }
+        else if (cmp > 0) {
+            x.right = put(x.right, key, val);
+        }
+        else {
+            x.val = val;
+        }
         x.N = 1 + size(x.left) + size(x.right);
         return x;
     }
 
-    /***********************************************************************
-     *  Delete
-     ***********************************************************************/
-
+    // Removes the smallest key and associated value from the symbol table.
     public void deleteMin() {
-        if (isEmpty()) throw new NoSuchElementException("Symbol table underflow");
+        if (isEmpty()) {
+            throw new NoSuchElementException("Symbol table underflow");
+        }
         root = deleteMin(root);
-        // assert check();
     }
 
     private Node deleteMin(Node x) {
-        if (x.left == null) return x.right;
+        if (x.left == null) {
+            return x.right;
+        }
         x.left = deleteMin(x.left);
         x.N = size(x.left) + size(x.right) + 1;
         return x;
     }
 
     public void deleteMax() {
-        if (isEmpty()) throw new NoSuchElementException("Symbol table underflow");
+        if (isEmpty()) {
+            throw new NoSuchElementException("Symbol table underflow");
+        }
         root = deleteMax(root);
-        // assert check();
     }
 
     private Node deleteMax(Node x) {
-        if (x.right == null) return x.left;
+        if (x.right == null) {
+            return x.left;
+        }
         x.right = deleteMax(x.right);
         x.N = size(x.left) + size(x.right) + 1;
         return x;
     }
 
     public void delete(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls delete() with a null key");
+        }
         root = delete(root, key);
         // assert check();
     }
 
     private Node delete(Node x, Key key) {
-        if (x == null) return null;
+        if (x == null) {
+            return null;
+        }
         int cmp = key.compareTo(x.key);
-        if (cmp < 0) x.left = delete(x.left, key);
-        else if (cmp > 0) x.right = delete(x.right, key);
+        if (cmp < 0) {
+            x.left = delete(x.left, key);
+        }
+        else if (cmp > 0) {
+            x.right = delete(x.right, key);
+        }
         else {
-            if (x.right == null) return x.left;
-            if (x.left == null) return x.right;
+            if (x.right == null) {
+                return x.left;
+            }
+            if (x.left == null) {
+                return x.right;
+            }
             Node t = x;
             x = min(t.right);
             x.right = deleteMin(t.right);
@@ -129,66 +159,96 @@ public class BST<Key extends Comparable<Key>, Value> {
         return x;
     }
 
-    /***********************************************************************
-     *  Min, max, floor, and ceiling
-     ***********************************************************************/
+
     public Key min() {
-        if (isEmpty()) return null;
+        if (isEmpty()) {
+            return null;
+        }
         return min(root).key;
     }
 
     private Node min(Node x) {
-        if (x.left == null) return x;
+        if (x.left == null) {
+            return x;
+        }
         else return min(x.left);
     }
 
     public Key max() {
-        if (isEmpty()) return null;
+        if (isEmpty()) {
+            return null;
+        }
         return max(root).key;
     }
 
     private Node max(Node x) {
-        if (x.right == null) return x;
+        if (x.right == null) {
+            return x;
+        }
         else return max(x.right);
     }
 
     public Key floor(Key key) {
         Node x = floor(root, key);
-        if (x == null) return null;
-        else return x.key;
+        if (x == null) {
+            return null;
+        }
+        else {
+            return x.key;
+        }
     }
 
     private Node floor(Node x, Key key) {
-        if (x == null) return null;
+        if (x == null) {
+            return null;
+        }
         int cmp = key.compareTo(x.key);
-        if (cmp == 0) return x;
-        if (cmp < 0) return floor(x.left, key);
+        if (cmp == 0) {
+            return x;
+        }
+        if (cmp < 0) {
+            return floor(x.left, key);
+        }
         Node t = floor(x.right, key);
-        if (t != null) return t;
-        else return x;
+        if (t != null) {
+            return t;
+        }
+        else {
+            return x;
+        }
     }
 
     public Key ceiling(Key key) {
         Node x = ceiling(root, key);
-        if (x == null) return null;
-        else return x.key;
+        if (x == null) {
+            return null;
+        }
+        else {
+            return x.key;
+        }
     }
 
     private Node ceiling(Node x, Key key) {
-        if (x == null) return null;
+        if (x == null) {
+            return null;
+        }
         int cmp = key.compareTo(x.key);
-        if (cmp == 0) return x;
+        if (cmp == 0) {
+            return x;
+        }
         if (cmp < 0) {
             Node t = ceiling(x.left, key);
-            if (t != null) return t;
-            else return x;
+            if (t != null) {
+                return t;
+            }
+            else {
+                return x;
+            }
         }
         return ceiling(x.right, key);
     }
 
-    /*****************************************************************************
-     *  Test client
-     *****************************************************************************/
+
     public static void main(String[] args) {
         BST<String, Integer> st = new BST<String, Integer>();
         for (int i = 0; !StdIn.isEmpty(); i++) {
@@ -201,3 +261,25 @@ public class BST<Key extends Comparable<Key>, Value> {
 
     }
 }
+
+
+/******************************************************************************
+ *  A symbol table implemented with a binary search tree.
+ *
+ *  % more tinyST.txt
+ *  S E A R C H E X A M P L E
+ *
+ *  % java BST < tinyST.txt
+ *  A 8
+ *  C 4
+ *  E 12
+ *  H 5
+ *  L 11
+ *  M 9
+ *  P 10
+ *  R 3
+ *  S 0
+ *  X 7
+ *
+ ******************************************************************************/
+// https://algs4.cs.princeton.edu/32bst/BST.java.html
--- Tutorial_03 (Searching with Trees)/Code/src/SearchST.java
+++ Tutorial_03 (Searching with Trees)/Code/src/SearchST.java
@@ -2,105 +2,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/*************************************************************************
- *  Expected output on the tinyST.txt example:
- *  L 11
- *  P 10
- *  M 9
- *  X 7
- *  H 5
- *  C 4
- *  R 3
- *  A 8
- *  E 12
- *  S 0
- *
- *************************************************************************/
-
-/****************************************************************************
- *  Compilation within a console or terminal:  
- *  Windows version: javac -cp .;stdlib.jar SearchST.java
- *  OS / Linux version: javac -cp .:stdlib.jar SearchST.java
- *
- *  Execution within a console or terminal:  
- *  Windows version: java -cp .;stdlib.jar SearchST < tinyST.txt
- *  OS / Linux version: javac -cp .:stdlib.jar SearchST < tinyST.txt
- *
- *  Adaptation: Enable and disable statements where appropriately in the lines of code below.
- *
- *  NOTE for execution within an IDE (i.e., Eclipse or NetBeans): In Eclipse, drag the corresponding input data file 
- *  tinyST.txt over the main project folder and COPY it; In NetBeans. copy-paste the tinyST.txt file into the NetBeans project
- *  directory. Accordingly, change the file name in the statement, for instance, reading data from the 
- *  tinyST.txt file:
- *
- *  input = new Scanner(new File("tinyST.txt"));
- *
- *  NOTE (2) for execution within an IDE: Do not forget to add the stdlib.jar to the Libraries.
- *
- *
- ****************************************************************************/
-
-/**
- * The <tt>SequentialSearchST</tt> class represents an (unordered)
- * symbol table of generic key-value pairs.
- * A symbol table implements the <em>associative array</em> abstraction:
- * when associating a value with a key that is already in the symbol table,
- * the convention is to replace the old value with the new value.
- * This implementation uses a singly-linked list and sequential search.
- * It relies on the <tt>equals()</tt> method to test whether two keys
- * are equal.
- */
-
 public class SearchST {
-    private int N;
-    private Node first;
 
-    private class Node {
-        private String key;
-        private Integer val;
-        private Node next;
-
-        public Node(String key, Integer val, Node next) {
-            this.key = key;
-            this.val = val;
-            this.next = next;
-        }
-    }
+    private int N;           // number of key-value pairs
+    private Node first;      // the first Node
 
     public SearchST() {
-    }
-
-
-    public int size() {
-        return N;
-    }
-
-    public boolean isEmpty() {
-        return size() == 0;
-    }
-
-    public boolean contains(String key) {
-        // Implement the method by inserting the missing statements...
-        return false;
-    }
-
-    public Integer get(String key) {
-        // Implement the method by inserting the missing statements...
-        return 0;
-    }
-
-    public void put(String key, Integer val) {
-        // Implement the method by inserting the missing statements...
-    }
-
-    public void delete(String key) {
-        first = delete(first, key);
-    }
-
-    private Node delete(Node x, String key) {
-        // Implement the method by inserting the missing statements...
-        // The implementation shall be using the DELETE methid above....
-        return null;
+        // Empty Constructor
     }
 
     public static void main(String[] args) {
@@ -109,7 +17,7 @@ public class SearchST {
         // ONLY for IDE version, remove otherwise
         Scanner input = null;
         try {
-            input = new Scanner(new File("D:/IIT/2nd Year/Alogrithm/Tutorials/Tutorial_03/tinyST.txt"));
+            input = new Scanner(new File("D:/IIT/2nd Year/Alogrithm/Tutorials/Tutorial_03 (Searching with Trees)/tinyST.txt"));
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -134,4 +42,179 @@ public class SearchST {
         // measure time taken to find.
 
     }
+
+    public int size() {
+        // Returns the number of key-value pairs in this symbol table.
+        return N;
+    }
+
+    public boolean isEmpty() {
+        // Returns true if this symbol table is empty.
+        return size() == 0;
+    }
+
+    public boolean contains(String key) {
+        // Returns true if this symbol table contains the specified key.
+        if (key == null) {
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+
+    public Integer get(String key) {
+        // Returns the value associated with the given key in this symbol table.
+        if (key == null) {
+            throw new IllegalArgumentException("argument to get() is null");
+        }
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key))
+                return x.val;
+        }
+        return null;
+    }
+
+    public void put(String key, Integer val) {
+        // 'Inserts' the specified key-value pair into the symbol table, 'overwriting the old '
+        // value with the 'new value 'if the symbol table 'already' contains the 'specified key.'
+        // 'Deletes' the specified key (and its associated value) from this symbol table
+        // if the specified value is {@code null}.
+
+        if (key == null) {
+            throw new IllegalArgumentException("first argument to put() is null");
+        }
+
+        if (val == null) {
+            delete(key);
+            return;
+        }
+
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+
+        first = new Node(key, val, first);
+        N++;
+    }
+
+    public void delete(String key) {
+        // Removes the specified key and its associated value from this symbol table.
+        // (if the key is in this symbol table).
+        if (key == null) {
+            throw new IllegalArgumentException("argument to delete() is null");
+        }
+        first = delete(first, key);
+    }
+
+    private Node delete(Node x, String key) {
+        // delete key in linked list beginning at Node x
+        //  warning: function call stack too large if table is large
+        if (x == null) return null;
+        if (key.equals(x.key)) {
+            N--;
+            return x.next;
+        }
+        x.next = delete(x.next, key);
+        return x;
+    }
+
+    // Node creating class
+    private class Node {
+        private String key;
+        private Integer val;
+        private Node next;
+
+        public Node(String key, Integer val, Node next) {
+            this.key = key;
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
+
+/**
+ * Expected output on the tinyST.txt example:
+ * L 11
+ * P 10
+ * M 9
+ * X 7
+ * H 5
+ * C 4
+ * R 3
+ * A 8
+ * E 12
+ * S 0
+ * <p>
+ * <p>
+ * The {SequentialSearchST} class represents an (unordered) symbol table of generic key-value pairs.
+ * It supports the usual 'put', 'get', 'contains','delete', 'size', and 'is-empty' methods.
+ * It also provides a 'keys' method for iterating over all of the keys.
+ * A symbol table implements the 'associative array' abstraction:
+ * when associating a value with a key that is already in the symbol table, the convention is to replace the
+ * old value with the new value.
+ * The class also uses the convention that values cannot be {null}. Setting the value associated with a key
+ * to {null} is equivalent to deleting the key from the symbol table.
+ * <p>
+ * It relies on the {equals()} method to test whether two keys are equal. It does not call either the {compareTo()} or
+ * {hashCode()} method.
+ * <p>
+ * This implementation uses a 's 'singly linked list' andequential search'.
+ * The 'put' and 'delete' operations take O(n).
+ * The 'get' and 'contains' operations takes O(n) time in the worst case.
+ * The 'size', and 'is-empty' operations take O(1) time.
+ * Construction takes O(1) time.
+ * <p>
+ * The {SequentialSearchST} class represents an (unordered) symbol table of generic key-value pairs.
+ * It supports the usual 'put', 'get', 'contains','delete', 'size', and 'is-empty' methods.
+ * It also provides a 'keys' method for iterating over all of the keys.
+ * A symbol table implements the 'associative array' abstraction:
+ * when associating a value with a key that is already in the symbol table, the convention is to replace the
+ * old value with the new value.
+ * The class also uses the convention that values cannot be {null}. Setting the value associated with a key
+ * to {null} is equivalent to deleting the key from the symbol table.
+ * It relies on the {equals()} method to test whether two keys are equal. It does not call either the {compareTo()} or
+ * {hashCode()} method.
+ * This implementation uses a 's 'singly linked list' andequential search'.
+ * The 'put' and 'delete' operations take O(n).
+ * The 'get' and 'contains' operations takes O(n) time in the worst case.
+ * The 'size', and 'is-empty' operations take O(1) time.
+ * Construction takes O(1) time.
+ * <p>
+ * The {SequentialSearchST} class represents an (unordered) symbol table of generic key-value pairs.
+ * It supports the usual 'put', 'get', 'contains','delete', 'size', and 'is-empty' methods.
+ * It also provides a 'keys' method for iterating over all of the keys.
+ * A symbol table implements the 'associative array' abstraction:
+ * when associating a value with a key that is already in the symbol table, the convention is to replace the
+ * old value with the new value.
+ * The class also uses the convention that values cannot be {null}. Setting the value associated with a key
+ * to {null} is equivalent to deleting the key from the symbol table.
+ * It relies on the {equals()} method to test whether two keys are equal. It does not call either the {compareTo()} or
+ * {hashCode()} method.
+ * This implementation uses a 's 'singly linked list' andequential search'.
+ * The 'put' and 'delete' operations take O(n).
+ * The 'get' and 'contains' operations takes O(n) time in the worst case.
+ * The 'size', and 'is-empty' operations take O(1) time.
+ * Construction takes O(1) time.
+ **/
+
+/**
+ *  The {SequentialSearchST} class represents an (unordered) symbol table of generic key-value pairs.
+ *  It supports the usual 'put', 'get', 'contains','delete', 'size', and 'is-empty' methods.
+ *  It also provides a 'keys' method for iterating over all of the keys.
+ *  A symbol table implements the 'associative array' abstraction:
+ *  when associating a value with a key that is already in the symbol table, the convention is to replace the
+ *  old value with the new value.
+ *  The class also uses the convention that values cannot be {null}. Setting the value associated with a key
+ *  to {null} is equivalent to deleting the key from the symbol table.
+ *  It relies on the {equals()} method to test whether two keys are equal. It does not call either the {compareTo()} or
+ *  {hashCode()} method.
+ *  This implementation uses a 's 'singly linked list' andequential search'.
+ *  The 'put' and 'delete' operations take O(n).
+ *  The 'get' and 'contains' operations takes O(n) time in the worst case.
+ *  The 'size', and 'is-empty' operations take O(1) time.
+ *  Construction takes O(1) time.
+ **/
+// https://algs4.cs.princeton.edu/31elementary/SequentialSearchST.java.html
+// https://algs4.cs.princeton.edu/30searching/
