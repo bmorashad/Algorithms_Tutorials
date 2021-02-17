33b4d9ec80b60548ff2121e6419c8bbbe32bfac9
Nazhim <nazhimkalam@gmail.com>
2021-02-17 10:47:16 +0530

    commit

---
+++ Tutorial_04 (Sorting Algorithms)/src/BubbleSort_Tutorial.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+
+public class BubbleSort_Tutorial {
+
+    public static void sort(int[] arr){
+        int n = arr.length;
+        int temp = 0;
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    //swap elements
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
+    }
+    
+    public static int[] randomValues(int howMany){
+        int[] result = new int[howMany];
+        Random random = new Random();
+        for(int i=0; i<howMany; i++)
+           result[i] = random.nextInt() % (10 * howMany);
+        return result;
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // How many values to generate
+        int numValues = 30;
+        // Whether to print results. Only use with small numbers of values.
+        boolean printResults = true; 
+        
+        
+        int[] a = randomValues(numValues);
+        if(printResults){
+            System.out.print("Before sorting: ");
+            for(int i=0;i<numValues; i++)
+                System.out.print(a[i] + " ");
+            System.out.println();
+        }
+        long start = System.currentTimeMillis();
+        sort(a);
+        long now = System.currentTimeMillis();
+        double elapsed = (now - start) / 1000.0;
+        if(printResults){
+            System.out.print("After sorting: ");
+            for(int i=0;i<numValues; i++)
+                System.out.print(a[i] + " ");
+            System.out.println();
+        }
+    	System.out.println();
+        System.out.println("Elapsed time = " + elapsed + " seconds");    
+    }    
+}
